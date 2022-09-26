b2320ec9339b90360f03a6026302eeb8f9d371cf
flattte <jakubwieczorek01@gmail.com>
2022-09-26 13:44:28 +0200

    better build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
-target="./program"
-if ! [[ -f "$target" ]]; then
-    rm ./program
-fi
-clang -O3 -Wall -march=native -lstdc++ main.cpp -o program
+#!/bin/bash
+compile_cpp() {
+    target="./program_cpp"
+    if ! [[ -f "$target" ]]; then
+        rm ./cpp_matmul
+    fi
+    clang -O3 -Wall -march=native -lstdc++ main.cpp -o cpp_matmul
+}
+
+compile_c() {
+    target="./program_c"
+    if ! [[ -f "$target" ]]; then
+        rm c_matmul
+    fi
+    clang -O3 -Wall -march=native cimpl.c -o c_matmul
+}
+
+case $1 in 
+    cpp)
+    compile_cpp
+    ;;
+    c)
+    compile_c
+    ;;
+    *)
+    compile_c
+    compile_cpp
+esac
